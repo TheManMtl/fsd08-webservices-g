0ec55b491b8f59a29a13ec28ff7904cd64f2db38
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-05-30 15:08:32 -0400

    bezkoder tutorials

--- nodejs-express-mysql/app/config/db.config.js
+++ nodejs-express-mysql/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "sql.freedb.tech",
-    USER: "freedb_karakush",
-    PASSWORD: "a$rxareJ63EnTR9",
-    DB: "freedb_webservice"
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DB: "day02bezkodertuterials"
   };
---
+++ nodejs-express-mysql/app/routes/tutorial.routes.js
@@ -0,0 +1,158 @@
+module.exports = app => {
+    const tutorials = require("../controllers/tutorial.controller.js");
+
+    var router = require("express").Router();
+
+    // Create a new Tutorial
+    router.post("/", tutorials.create);
+
+    // Retrieve all Tutorials
+    router.get("/", tutorials.findAll);
+
+    // Retrieve all published Tutorials
+    router.get("/published", tutorials.findAllPublished);
+
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
+
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
+
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+
+    // Delete all Tutorials
+    router.delete("/", tutorials.deleteAll);
+
+    app.use('/api/tutorials', router);
+};
+
+//Create a new object
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    // Create a Tutorial
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published || false
+    });
+
+    // Save Tutorial in the database
+    Tutorial.create(tutorial, (err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Tutorial."
+            });
+        else res.send(data);
+    });
+};
+
+// Retrieve all Tutorials from the database (with condition).
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+
+    Tutorial.getAll(title, (err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        else res.send(data);
+    });
+};
+
+exports.findAllPublished = (req, res) => {
+    Tutorial.getAllPublished((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        else res.send(data);
+    });
+};
+
+exports.findOne = (req, res) => {
+    Tutorial.findById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Tutorial with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Tutorial with id " + req.params.id
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+
+//Update an object
+
+exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+  
+    console.log(req.body);
+  
+    Tutorial.updateById(
+      req.params.id,
+      new Tutorial(req.body),
+      (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found Tutorial with id ${req.params.id}.`
+            });
+          } else {
+            res.status(500).send({
+              message: "Error updating Tutorial with id " + req.params.id
+            });
+          }
+        } else res.send(data);
+      }
+    );
+  };
+//Delete an object
+
+exports.delete = (req, res) => {
+    Tutorial.remove(req.params.id, (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Tutorial with id ${req.params.id}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Could not delete Tutorial with id " + req.params.id
+          });
+        }
+      } else res.send({ message: `Tutorial was deleted successfully!` });
+    });
+  };
+
+  //Delete all objects
+
+exports.deleteAll = (req, res) => {
+  Tutorial.removeAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    else res.send({ message: `All Tutorials were deleted successfully!` });
+  });
+};
