a319854fc2fc553b35f14f3ea8f06c57da5da2ab
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-05-30 22:36:10 -0400

    todos routers

---
+++ day02todos/app/routes/todos.routes.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+    const todos = require("../controllers/todos.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Todos
+    router.post("/", todos.create);
+  
+    // Retrieve all ToDos
+    router.get("/", todos.findAll);
+  
+    // Retrieve a single ToDos with id
+    router.get("/:id", todos.findOne);
+  
+    // Update a ToDo with id
+    router.put("/:id", todos.update);
+  
+    // Delete a ToDo with id
+    router.delete("/:id", todos.delete);
+  
+    app.use('/api/ToDos', router);
+  };
